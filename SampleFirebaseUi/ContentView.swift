e33243e705faf7c44712629d568048ded1b1a3ee
zunema <yxxa.abc@gmail.com>
2023-08-20 00:26:24 +0900

    Initial Commit

---
+++ SampleFirebaseUi/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SampleFirebaseUi
+//
+//  Created by 飯尾悠也 on 2023/08/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SampleFirebaseUi/SampleFirebaseUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleFirebaseUiApp.swift
+//  SampleFirebaseUi
+//
+//  Created by 飯尾悠也 on 2023/08/20.
+//
+
+import SwiftUI
+
+@main
+struct SampleFirebaseUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
